596fea883d2dfd2042522baa11ca1a815c75fd63
Felipe Saenz <saenzfelipe2001@gmail.com>
2024-05-13 00:23:31 -0500

    Create a logic for import xlsx file and see the data on a table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
+  <nav class="bg-primary text-center ">
+    <router-link to="/">Subir Excel</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
@@ -11,17 +11,17 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 nav {
   padding: 30px;
+  color: #fff;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
 }
 
 nav a.router-link-exact-active {
---
+++ src/views/ExcelView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class=" mt-5 text-center text-xl font-bold mb-5">
+    <h1>Hola bom dia</h1>
+  </div>
+  <div class="w-full flex ">
+    <div class="w-1/3 border p-5 mr-2 rounded-lg border-black max">
+      <h1 class="text-xl font-bold">Subir Excel</h1>
+      <p>Pequeño Formulario para subir el excel</p>
+      <hr>
+      <div class="flex flex-col my-5">
+            <label class="w-ful mb-2 font-bold" >Archivo</label>
+            <input @change="handleFileChange" class="w-ful bg-gray-200 text-black rounded-md h-12 p-2" type="file"  >
+      </div>
+      <div class="flex justify-center mt-5 text-white">
+        <button @click="handleFileUpload" class="bg-green-500/100 hover:bg-green-600/100 w-1/5 h-8 rounded-md font-bold">Guardar</button>
+      </div>
+
+    </div>
+    <div class="w-1/2 border ml-2">
+      <table v-if="(excelData)" class="w-full text-sm rtl:text-right text-gray-200">
+        <thead class="text-xs text-center uppercase bg-gray-700  text-gray-400">
+            <tr>
+                <th v-for="(value, key) in excelData[0]" :key="key" scope="col" class="px-6 py-3">{{ value }}</th>
+            </tr>
+        </thead>
+        <tbody class="text-left">
+            <tr v-for="(row, index) in excelData.slice(1)" :key="index" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td v-for="(value, key) in row" :key="key" scope="row" class="px-6 py-4 font-medium whitespace-nowrap ">{{ value }}</td>
+            </tr>
+
+        </tbody>
+        </table>
+    </div>
+  </div>
+</template> 
+
+<script setup>
+import { ref } from 'vue';
+import * as XLSX from 'xlsx';
+const file = ref(null);
+const excelData = ref(null);
+
+const handleFileChange = (event) => {
+  file.value = event.target.files[0];
+  handleFileUpload();
+};
+
+const handleFileUpload = () => {
+  if (!file.value) {
+    console.error('No se ha seleccionado ningún archivo.');
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: 'array' });
+
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]]; // Solo toma la primera hoja del archivo Excel
+    const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+    excelData.value = rows; // Almacena los datos del archivo Excel en la variable excelData
+
+   
+    console.log('Datos del archivo Excel:', excelData.value); // Imprimir los primeros 5 elementos
+    // Aquí puedes guardar el JSON en alguna variable o enviarlo a alguna función para su procesamiento
+  };
+  reader.readAsArrayBuffer(file.value);
+};
+
+</script>
